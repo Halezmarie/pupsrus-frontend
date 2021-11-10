9e3f1bcc24fc429b2084240bc99707423c0bdd5b
halezmarie <teacherhaleymarie@gmail.com>
2021-11-10 12:17:58 -0600

    Wrap app into provider so the store is global, adding in store as well and notes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import App from './App';
 
+//set up our store so that means I need to import create store from REDUX
+//any component I wrap in provider will have access to the redux store
+let store = createStore(reducer, applyMiddleware(thunk))
 
+
+
+
+// Have to wrap the app and provider so that the store is global to all the other components/containers that I create 
 ReactDOM.render(
-    <App />,
+  <Provider>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
 
