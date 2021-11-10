49da05340c092b28e4a9b89fc269162395a38e21
halezmarie <teacherhaleymarie@gmail.com>
2021-11-10 15:57:54 -0600

    Set up functional comp for toy list

---
+++ src/components/ToyList.js
@@ -0,0 +1,14 @@
+import React from 'core-js/library/fn/reflect/es7/metadata'
+// I need 5 functional components 
+
+// rendering a list of toys
+const Toys = (props) => {
+
+    return (
+<div>
+    Toys
+</div>
+
+    )
+}
+export default Toys 
