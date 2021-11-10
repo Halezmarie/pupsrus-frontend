151d5f59c00dfade487e076ad59d8a6193d8a20e
halezmarie <teacherhaleymarie@gmail.com>
2021-11-10 15:43:13 -0600

    Getting toy form set up

---
+++ src/components/ToyForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class ToyForm extends React.Component {
+
+    render() {
+        return (
+       <div>
+           TestingToyForm
+       </div>
+        )
+    }
+}
+
+export default ToyForm
