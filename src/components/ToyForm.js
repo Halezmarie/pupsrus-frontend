0cd5de09eb19d795856e08b656a6ae94f50a215a
halezmarie <teacherhaleymarie@gmail.com>
2021-11-12 18:20:55 -0600

    added local state

--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,18 +1,23 @@
 import React from 'react'
 
     class ToyForm extends React.Component {
-
-
+        state = {
+            name: '',
+            description: '',
+            image_url: ''
+          }
+  //leaving it local 
+  
     render() {
         return (
        <div>
            <form>
                <label>Name: </label>
-               <input type='text' placeholder='Toy name...' /><br/>
+               <input type='text' placeholder='Toy name...' value={this.state.name} /><br/>
                <label>Description </label>
-               <input type='text' placeholder='Description of the toy...' /><br/>
+               <input type='text' placeholder='Description of the toy...' value={this.state.description} /><br/>
                <label>Image </label>
-               <input type='text' placeholder='Picture of the toy...' /><br/>
+               <input type='text' placeholder='Picture of the toy...' value={this.state.image_url} /><br/>
                
            </form>
        </div>
