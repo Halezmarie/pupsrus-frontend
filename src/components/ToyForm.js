3311cde4e4c505662a0b06f7e0d165bc613e54c5
halezmarie <teacherhaleymarie@gmail.com>
2021-11-12 18:29:14 -0600

    Making sure that the event is being hit each time

--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -7,17 +7,26 @@ import React from 'react'
             image_url: ''
           }
   //leaving it local 
-  
+          
+
+
+    handleChange = (event) => {
+
+        debugger;
+    }
+    
+
+
     render() {
         return (
        <div>
            <form>
                <label>Name: </label>
-               <input type='text' placeholder='Toy name...' value={this.state.name} /><br/>
+               <input type='text' placeholder='Toy name...' value={this.state.name} name="name" onChange={this.handleChange} /><br/>
                <label>Description </label>
-               <input type='text' placeholder='Description of the toy...' value={this.state.description} /><br/>
+               <input type='text' placeholder='Description of the toy...' value={this.state.description} name="description" onChange={this.handleChange} /><br/>
                <label>Image </label>
-               <input type='text' placeholder='Picture of the toy...' value={this.state.image_url} /><br/>
+               <input type='text' placeholder='Picture of the toy...' value={this.state.image_url} name="image" onChange={this.handleChange}/><br/>
                
            </form>
        </div>
