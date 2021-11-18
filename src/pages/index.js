754fdb38856824ef630f2359ce3c25568e7511c9
halezmarie <teacherhaleymarie@gmail.com>
2021-11-18 16:30:05 -0600

    Formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,12 @@ const Home = () => {
               display: 'flex',
               justifyContent: 'center',
               alignItems: 'center',
-              height: '100vh'
+              height: '80vh'
           }}
       >
           <h1>Welcome to Pups R Us!🐶 </h1>
           <div>
-              <img src="https://images.unsplash.com/photo-1587559070757-f72a388edbba?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZG9nJTIwdG95fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="display pic" />
+              <img src="https://puppytoob.com/wp-content/uploads/2017/08/Toys.jpg" alt="display pic" />
           </div>
 
       </div>
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -5,10 +5,10 @@ function SignIn() {
         <div>
             <h1>Sign in so you can get started! (coming soon) </h1>
             <div>
-              <img src="https://puppytoob.com/wp-content/uploads/2017/08/Toys.jpg" alt="display pic" />
+              <img src="https://images.unsplash.com/photo-1587559070757-f72a388edbba?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZG9nJTIwdG95fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="display pic" />
           </div>
         </div>
     )
-}
+    }
 
 export default SignIn;
