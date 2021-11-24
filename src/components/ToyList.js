66d5c4d12ee4ec987d1b0ec8013ad08212bd936a
halezmarie <teacherhaleymarie@gmail.com>
2021-11-24 15:12:45 -0600

    Styling for the toylist so it looks better

--- src/components/ToyList.js
+++ src/components/ToyList.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {Container, Card, Row, Col} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css';
 
 // rendering a list of toys
 
@@ -8,15 +10,20 @@ const ToyList = (props) => {
     // iterating over my data and making a list to display to the user
     return (
         <div>
+        <Container fluid>
+        <Row>
           {props.toys.map(toy =>
-            <div key={toy.id}>
-              <Link to={`/toys/${toy.id}`}>{toy.name} -  <img src={toy.image_url} alt="toyimage" width="300" height="300" /></Link>
-            </div> )}
+            <Col key={toy.id}>
+                <Card style={{ width: '17rem' }} className='text-center'>
+              <Link to={`/toys/${toy.id}`}>{toy.name}
+              <Card.Img variant='top' src={toy.image_url} alt="toyimage" width="300" height="300" /></Link>
+              </Card>
+            </Col>
+          )}
+        </Row>
+      </Container>     
         </div>
-    
-      )
-    }
+    )}
 // need props first because it is the info being passed from the toy container to my child component of toys
 
-export default ToyList
-
+export default ToyList;
