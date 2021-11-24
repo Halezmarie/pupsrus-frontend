8947c330db4f3f34b08a3f9921a844d6201f2214
halezmarie <teacherhaleymarie@gmail.com>
2021-11-24 15:44:30 -0600

    Styling on displaying the toy

--- src/components/DisplayToy.js
+++ src/components/DisplayToy.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReviewsContainer from '../containers/ReviewsContainer'
+import {Container} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css';
 //functional not doing anything with state
 
 const DisplayToy = (props) => {
@@ -7,13 +9,15 @@ const DisplayToy = (props) => {
 let toy = props.toys.filter(toy => toy.id == props.match.params.id)[0]
 
     return (
-    <div>
+        <Container>
     <h1>
-        <img src={toy? toy.image_url : null} alt="toyimage" width="300" height="300"/><br></br>
-        {toy ? toy.name : null} : {toy ? toy.description : null} 
+        <h1> {toy ? toy.name : null}</h1>
+        <img src={toy? toy.image_url : null} alt="toyimage" width="300" height="300" /><br></br>
+          <p>{toy ? toy.description : null}</p>
         <ReviewsContainer toy={toy}/>
     </h1>
-    </div>
+    </Container>
+  
     )
 }
 
