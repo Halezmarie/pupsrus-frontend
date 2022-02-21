37f38e081dd1fe4f6d4a8beadb375ead8a190955
Halezmarie <teacherhaleymarie@gmail.com>
2022-02-20 23:57:43 -0600

    Setting up an action to delete toys

---
+++ src/actions/deleteToy.js
@@ -0,0 +1,12 @@
+export const deleteToy = (toyId, history) => {
+    return(dispatch) => {
+      fetch(`http://localhost:3000/api/v1/toys/${toyId}`, {
+        method: 'DELETE'
+      })
+        .then(response => response.json())
+        .then(toyId => {
+          dispatch({type: 'DELETE_TOY', payload: toyId })
+          history.push('/toys')
+        })
+    }
+  }
