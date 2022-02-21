c0a0a05047b583e5ea3eafbce4be2442914b6a41
Halezmarie <teacherhaleymarie@gmail.com>
2022-02-21 00:17:05 -0600

    Button for delete

--- src/components/DisplayToy.js
+++ src/components/DisplayToy.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReviewsContainer from '../containers/ReviewsContainer'
-import { Container } from 'react-bootstrap'
+import { Container, Button } from 'react-bootstrap'
 import {connect} from 'react-redux'
 import {deleteToy} from '../actions/deleteToy.js';
 
@@ -8,7 +8,7 @@ const DisplayToy = (props) => {
 
     let toy = props.toys.filter(toy => toy.id == props.match.params.id)[0]
     if (!toy) return null
-    let toyInfo = (toy &&toy[0].attributes);
+    // let toyInfo = (toy &&toy[0]);
 
     const handleDelete = (toyInfo) => {
         props.deleteToy(toyInfo.id, props.history)
@@ -21,6 +21,7 @@ const DisplayToy = (props) => {
                 <h1> {toy.name}</h1>
                 <img src={toy.image_url} alt="toyimage" width="300" height="300" /><br></br>
                 <p>{toy.description}</p>
+                <Button onClick={() => handleDelete(toyInfo)}>Delete Toy</Button>
                 <ReviewsContainer toy={toy} />
             </h1>
         </Container>
