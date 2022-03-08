ed04296e4133bc04c9394e327e9ad425dc137972
Halezmarie <teacherhaleymarie@gmail.com>
2022-03-07 18:57:54 -0600

    Review form editing

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -11,7 +11,7 @@ const Reviews = (props) => {
   return (
     <div>
       {props.reviews && props.reviews.map(review =>
-        <div key={review.id}> <br></br> <br></br>  Title: {review.title}<br></br> Rating: {review.rating}<br></br>{review.content} <button onClick={() => handleDelete(review)}>Delete</button></div>
+        <div key={review.id}> <br></br> <br></br>  Title: {review.title}<br></br> Rating: {review.rating}<br></br>{review.content} <button onClick={() => handleDelete(review)}>Delete Review</button></div>
       )}
     </div>
   )
