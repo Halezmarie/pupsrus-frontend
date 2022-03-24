a12444ee100734f44713d37e0984592f0ab15427
Halezmarie <teacherhaleymarie@gmail.com>
2022-03-23 21:12:15 -0500

    Commented out search bar for now for cloning

--- src/components/SearchToys.js
+++ src/components/SearchToys.js
@@ -1,30 +1,30 @@
-const Search = (props) => {
+// const Search = (props) => {
 
-// search and query 
-  const toys = props.toys
-}
-
-
-
-// filter toys, pass in props of toys and query. 
-
-const filterToys = ((toys, query) => {
-  constToyName = toy.attributes.name.toLowerCase()
-  return toyName.includes(query)
-})
-
-const filteredToys = filterToys(toys, query)
-// return the toys as a filter with the name to lowercase
-return (
-    <div>
-    <form action='/' method='get'>
-      <label htmlFor='toy-search'>Search for a toy...: </label>
-      <input type='text' id='toysearch' placeholder='Name' />
-      <button type="submit">Search</button>
-    </form>
-    </div>
-  )
+// // search and query 
+//   const toys = props.toys
+// }
 
 
 
-export default Search;
+// // filter toys, pass in props of toys and query. 
+
+// const filterToys = ((toys, query) => {
+//   constToyName = toy.attributes.name.toLowerCase()
+//   return toyName.includes(query)
+// })
+
+// const filteredToys = filterToys(toys, query)
+// // return the toys as a filter with the name to lowercase
+// return (
+//     <div>
+//     <form action='/' method='get'>
+//       <label htmlFor='toy-search'>Search for a toy...: </label>
+//       <input type='text' id='toysearch' placeholder='Name' />
+//       <button type="submit">Search</button>
+//     </form>
+//     </div>
+//   )
+
+
+
+// export default Search;
