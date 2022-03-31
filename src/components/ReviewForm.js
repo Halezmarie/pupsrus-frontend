7e709a25d9e7e062f977915e01072b5813e24584
Halezmarie <teacherhaleymarie@gmail.com>
2022-03-30 22:31:49 -0500

    Working on modernizing the reviewform component

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,50 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { createReview } from '../actions/createReview'
 
-class ReviewForm extends React.Component {
-    state = {
+
+function ReviewForm (props) {
+    const [form, setValues] = useState({
         title: '',
         rating: '',
-        content: ''
-    }
+        content: ''    
+})
 
-    handleChange = (event) => {
-        this.setState({
+   const handleChange = (event) => {
+        setValues({
             [event.target.name]: event.target.value
         })
     }
 
-    handleSubmit = (event) => {
+   const handleSubmit = (event) => {
         event.preventDefault()
-        this.props.createReview(this.state, this.props.toy.id)
-        this.setState({
+        props.createReview(props.toy.id)
+        setValues({
+            ...form,
             title: '',
             rating: '',
             content: ''
         })
-    }
+   }
 
-    render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={handleSubmit}>
                     <br></br>
                     <text> Add a review for this product: </text>
                     <br></br><br></br>
                     <label> Review Title: </label>
-                    <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
+                    <input type="text" name="title" value={setValues.title} onChange={handleChange} />
                     <br></br><br></br>
                     <label> Rating: </label>
-                    <input type="number" name="rating" min="0" max="10" value={this.state.rating} onChange={this.handleChange} />
+                    <input type="number" name="rating" min="0" max="10" value={setValues.rating} onChange={handleChange} />
                     <br></br><br></br>
                     <label> Review Content: </label>
-                    <input type="text" name="content" value={this.state.content} onChange={this.handleChange}  />
+                    <input type="text" name="content" value={setValues.content} onChange={handleChange}  />
                     <br></br><br></br>
                     <input type="submit" />
                 </form>
             </div>
         )
     }
-}
+
 export default connect(null, { createReview })(ReviewForm)
