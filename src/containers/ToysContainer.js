eec3fca5cce91a856251ed265ed6e4e1b57841f0
Halezmarie <teacherhaleymarie@gmail.com>
2022-04-05 21:59:04 -0500

    Notes

--- src/containers/ToysContainer.js
+++ src/containers/ToysContainer.js
@@ -8,11 +8,16 @@ import DisplayToy from '../components/DisplayToy'
 import ToyForm from '../components/ToyForm'
 
 
+//By implementing useDispatch, we are able to dispatch any action to the redux store by adding an action as an argument to the new variable
 const ToysContainer = () => {
   const dispatch = useDispatch();
   dispatch(fetchToys());
 
   const toys = useSelector((state) => state.toys);
+
+  // takes the current state as an argument and returns the data you want to retrieve from it. But wait, there’s more! 
+  //This crafty little hook also lets you store the return values inside a variable within the scope of the functional components instead of 
+  //having to pass it down as props. So instead of using mapStateToProps, we will be implementing this neat hook instead!
   
   return(
     <div>
