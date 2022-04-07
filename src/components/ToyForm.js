76d2b2f54d17fa9e6a2a4039274a976823a5f6bc
Halezmarie <teacherhaleymarie@gmail.com>
2022-04-07 02:51:20 -0500

    Formatting

--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
-import { createToy } from "../actions/createToy";
+import { createToy } from '../actions/createToy';
 
 const ToyForm = ({ toy }) => {
   const dispatch = useDispatch();
-  const [form, setForm] = useState({ 
-    name: "",
-    description: "",
-    image_url: "",
+  const [form, setForm] = useState({
+    name: '',
+    description: '',
+    image_url: '',
   });
 
   const handleChange = (event) => {
@@ -23,45 +23,43 @@ const ToyForm = ({ toy }) => {
     dispatch(createToy(form, toy));
     setForm({
       ...form,
-      name: "",
-      description: "",
-      image_url: "",
+      name: '',
+      description: '',
+      image_url: '',
     });
   };
 
-    return (
-      <Container className="w-25">
-        <h1 className="header text-center">Create a New Toy 🐶🥎</h1>
-        <Form onSubmit={handleSubmit}>
+  return (
+    <Container className="w-25">
+      <h1 className="header text-center">Create a New Toy 🐶🥎</h1>
+      <Form onSubmit={handleSubmit}>
         <Form.Label>Title:</Form.Label>
         <Form.Control
-              type="text"
-              placeholder="Toy name..."
-              value={form.name}
-              name="name"
-              onChange={handleChange}
-            />
-           <Form.Label>Description:</Form.Label>
+          type="text"
+          placeholder="Toy name..."
+          value={form.name}
+          name="name"
+          onChange={handleChange}
+        />
+        <Form.Label>Description:</Form.Label>
         <Form.Control
-              type="text"
-              placeholder="Description of the toy..."
-              value={form.description}
-              name="description"
-              onChange={handleChange}
-            />
-          <Form.Label>Picture:</Form.Label>
+          type="text"
+          placeholder="Description of the toy..."
+          value={form.description}
+          name="description"
+          onChange={handleChange}
+        />
+        <Form.Label>Picture:</Form.Label>
         <Form.Control
-              type="text"
-              placeholder="Picture of the toy..."
-              value={form.image_url}
-              name="image_url"
-              onChange={handleChange}
-            />
-          <Button variant="primary" type="submit">
-          Submit
-        </Button>
+          type="text"
+          placeholder="Picture of the toy..."
+          value={form.image_url}
+          name="image_url"
+          onChange={handleChange}
+        />
+        <Button variant="primary" type="submit">Submit</Button>
       </Form>
     </Container>
   );
-    }
+};
 export default ToyForm;
