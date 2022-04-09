f06a527df48e959308b5cde9971ad56d8f3c5509
Halezmarie <teacherhaleymarie@gmail.com>
2022-04-09 02:50:14 -0500

    I added the history hook and got rid of the display/show

--- src/actions/deleteToy.js
+++ src/actions/deleteToy.js
@@ -1,4 +1,4 @@
-export const deleteToy = (toyId) => {
+export const deleteToy = (toyId, history) => {
   return (dispatch) => {
     fetch(`http://localhost:3000/api/v1/toys/${toyId}`, {
       method: "DELETE",
@@ -6,6 +6,7 @@ export const deleteToy = (toyId) => {
       .then((response) => response.json())
       .then((toyId) => {
         dispatch({ type: "DELETE_TOY", payload: toyId });
+        history.push('/toys')
       })
       .catch((err) => alert(err));
   };
--- src/components/DisplayToy.js
+++ src/components/DisplayToy.js
@@ -1,35 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReviewsContainer from '../containers/ReviewsContainer';
 import { Container, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteToy } from '../actions/deleteToy.js';
 import { selectToy } from '../selectors/toySelectors';
+import { useHistory } from 'react-router';
 
 const DisplayToy = (props) => {
-  const [display, setDisplay] = useState(true);
-  const [show, setShow] = useState(false);
   const dispatch = useDispatch();
 
   const toy = useSelector(selectToy(props.match.params.id));
 
+  const history = useHistory();
+
   const handleDelete = (toyId) => {
-    dispatch(deleteToy(toyId));
-    setDisplay(false);
-    setShow(true);
+    dispatch(deleteToy(toyId, history));
   };
 
   return (
     <Container>
-      {display && (
-        <div>
-          <h1 className="header text-center">{toy.name}</h1>
-          <img src={toy.image_url} alt="toy-image" width="300" height="300"/>
-          <p>{toy.description}</p>
-          <Button onClick={() => handleDelete(toy.id)}>Delete Toy</Button>
-          <ReviewsContainer toy={toy}/>
-        </div>
-      )}
-      {show && <p>Toy deleted successfully</p>}
+      <div>
+        <h1 className="header text-center">{toy.name}</h1>
+        <img src={toy.imageUrl} alt="toy-image" width="300" height="300" />
+        <p>{toy.description}</p>
+        <Button onClick={() => handleDelete(toy.id)}>Delete Toy</Button>
+        <ReviewsContainer toy={toy} />
+      </div>
+      )
     </Container>
   );
 };
