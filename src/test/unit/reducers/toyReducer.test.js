5b00b0b91ea06535f1a3f4d2c6a4665e6142ad7d
ssteele <ssteele@godaddy.com>
2022-04-10 14:21:29 -0700

    toy id is a number

--- src/test/unit/reducers/toyReducer.test.js
+++ src/test/unit/reducers/toyReducer.test.js
@@ -3,15 +3,15 @@ import { toyWithReview } from '../../fixtures/toys';
 
 describe('toyReducer', () => {
   let state;
-  
-  beforeEach(() => { 
+
+  beforeEach(() => {
     state = initialState;
   });
-  
+
   it('should handle initial state', () => {
     expect(toyReducer(undefined, {})).toEqual(initialState);
   });
-  
+
   it('should handle FETCH_TOYS', () => {
     const payload = 'any-payload';
     const action = { type: 'FETCH_TOYS', payload };
@@ -24,34 +24,34 @@ describe('toyReducer', () => {
     const payload = 'any-payload';
     const action = { type: 'CREATE_TOY', payload};
     const expected = { toys: [payload] };
-    
+
     expect(toyReducer(state, action)).toEqual(expected);
   });
 
   it('should handle CREATE_REVIEW', () => {
     state = { toys: [{ ...toyWithReview, reviews: [] }] };
-    
+
     const payload = toyWithReview;
     const action = { type: 'CREATE_REVIEW', payload};
     const expected = { toys: [toyWithReview] };
-    
+
     expect(toyReducer(state, action)).toEqual(expected);
   });
 
   it('should handle DELETE_REVIEW', () => {
     state = { toys: [toyWithReview] };
-    
+
     const payload = { ...toyWithReview, reviews: [] }
     const action = { type: 'DELETE_REVIEW', payload };
     const expected = { toys: [{ ...toyWithReview, reviews: [] }]};
-    
+
     expect(toyReducer(state, action)).toEqual(expected);
   });
 
   it('should handle DELETE_TOY', () => {
     state = { toys: [toyWithReview] };
 
-    const payload = '1';
+    const payload = 1;
     const action = { type: 'DELETE_TOY', payload };
     const expected = initialState;
 
