57bb74171cf4c5e1b30da13e399ca1c0afe1980f
ssteele <ssteele@godaddy.com>
2022-04-10 20:39:10 -0700

    fix Review props

--- src/components/Toys/Review.js
+++ src/components/Toys/Review.js
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux';
 import { Button, Card } from 'react-bootstrap';
 import { deleteReview } from '../../actions/deleteReview';
 
-const Review = ({ id, content, rating, title, toy_id }) => {
+const Review = ({ review: { id, content, rating, title, toy_id }}) => {
   const dispatch = useDispatch();
 
   return (
