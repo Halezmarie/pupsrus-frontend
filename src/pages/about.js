940fb0574bc18858488a7f7d3780115cce1ab3f9
Halezmarie <teacherhaleymarie@gmail.com>
2022-04-10 19:27:00 -0500

    Quick css edits and formatting

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,30 @@
-import React from "react"
-import '../style.css'
+import React from 'react';
+import '../style.css';
 
 const About = () => {
-    return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh'
-            }}
-        >
-            <h1>Pups R Us is dedicated to giving you the best experience while you are trying to figure out which toy to buy your dog next! You can add or review toys, or you can just browse around.</h1>
-            <div>
-                <img className='anotherpuppy' src="https://media.istockphoto.com/photos/dog-chooses-and-fetches-rope-toy-from-hoard-of-pet-toys-in-cart-picture-id1149531683?k=20&m=1149531683&s=612x612&w=0&h=_13gN6KH41CWZJXXrkkx2D_rQeNJbHaTNrcOUa5v1uo=" alt="display pic" />
-            </div>
-
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <h1>
+        Pups R Us is dedicated to giving you the best experience while you are
+        trying to figure out which toy to buy your dog next! You can add or
+        review toys, or you can just browse around.
+      </h1>
+      <div>
+        <img
+          className="puppy"
+          src="https://media.istockphoto.com/photos/dog-chooses-and-fetches-rope-toy-from-hoard-of-pet-toys-in-cart-picture-id1149531683?k=20&m=1149531683&s=612x612&w=0&h=_13gN6KH41CWZJXXrkkx2D_rQeNJbHaTNrcOUa5v1uo="
+          alt="display pic"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import '../style.css'
+import React from 'react';
+import '../style.css';
 
-function SignUp() {
-    return (
-        <div>
-            <h1>Sign up and join in all the fun of Pups R Us! (coming soon)</h1>
+const SignUp = () => {
+  return (
+    <div>
+      <h1>Sign up and join in all the fun of Pups R Us! (coming soon)</h1>
 
-            <div>
-              <img className='yetanotherpuppy' src="https://images.unsplash.com/photo-1589924749359-9697080c3577?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9nJTIwdG95fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="display pic" />
-          </div>
-        </div>
-    )
-}
+      <div>
+        <img
+          className="puppy"
+          src="https://images.unsplash.com/photo-1589924749359-9697080c3577?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9nJTIwdG95fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
+          alt="display pic"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default SignUp;
