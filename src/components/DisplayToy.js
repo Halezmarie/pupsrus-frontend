0c76d5830e1799ecfa99f53143e21f578a97a902
Halezmarie <teacherhaleymarie@gmail.com>
2022-04-10 20:54:51 -0500

    Fixed an issue I had with the toy name being undefined

--- src/components/DisplayToy.js
+++ src/components/DisplayToy.js
@@ -4,30 +4,32 @@ import { Container, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteToy } from '../actions/deleteToy.js';
 import { selectToy } from '../selectors/toySelectors';
-import { useHistory } from 'react-router';
 import { useParams } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 const DisplayToy = () => {
   const dispatch = useDispatch();
-  const {id} = useParams();
+  const { id } = useParams();
 
   const toy = useSelector(selectToy(id));
 
-  const history = useHistory();
-
-  return (
-    <Container>
-      <div>
-        <h1 className="header text-center">{toy.name}</h1>
-        <img src={toy.image_url} alt="toy-image" width="300" height="300" />
-        <p>{toy.description}</p>
-        <Button onClick={() => dispatch(deleteToy(toy.id, history))}>
-          Delete Toy
-        </Button>
-        <ReviewsContainer toy={toy} />
-      </div>
-    </Container>
-  );
+  if (toy)
+    return (
+      <Container>
+        <div>
+          <h1 className="header text-center">{toy.name}</h1>
+          <img src={toy.image_url} alt="toy-image" width="300" height="300" />
+          <p>{toy.description}</p>
+          <Button onClick={() => dispatch(deleteToy(toy.id))}>
+            Delete Toy
+          </Button>
+          <ReviewsContainer toy={toy} />
+        </div>
+      </Container>
+    );
+  else {
+    return <Redirect to="/toys" />;
+  }
 };
 
 export default DisplayToy;
